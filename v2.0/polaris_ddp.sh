c4274e90ab2d0b7c60589cdea2fc72e7b998eb3b
himanshisyadav <himanshimj@gmail.com>
2024-05-21 16:01:16 +0000

    polaris_ddp.sh updated

--- v2.0/polaris_ddp.sh
+++ v2.0/polaris_ddp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 #PBS -N random
-#PBS -l select=1:system=polaris:ngpus=4:gputype=A100
+#PBS -l select=1:system=polaris:ncpus=64:ngpus=4:gputype=A100
 #PBS -l place=scatter
 #PBS -q debug 
 #PBS -l walltime=00:10:00
@@ -13,15 +13,21 @@
 export MPICH_GPU_SUPPORT_ENABLED=1
 
 # Change to working directory
-cd ${PBS_O_WORKDIR}
+cd $PBS_O_WORKDIR
 
 # MPI and OpenMP settings
 NNODES=`wc -l < $PBS_NODEFILE`
-NRANKS_PER_NODE=$(nvidia-smi -L | wc -l)
-NDEPTH=8
-NTHREADS=1
+#Follwing will be the number of GPUs on each node, so 4 in our case as each node has 4 GPUs
+NUM_TASKS_PER_NODE= $(nvidia-smi -L | wc -l)
+WORLD_SIZE=$((NNODES * NUM_TASKS_PER_NODE))
 
-NTOTRANKS=$(( NNODES * NRANKS_PER_NODE ))
-echo "NUM_OF_NODES= ${NNODES} TOTAL_NUM_RANKS= ${NTOTRANKS} RANKS_PER_NODE= ${NRANKS_PER_NODE} THREADS_PER_RANK= ${NTHREADS}"
+echo "NUM_OF_NODES= ${NNODES} NUM_TASKS_PER_NODE= ${NUM_TASKS_PER_NODE} WORLD_SIZE= ${WORLD_SIZE}"
 
-mpiexec -n ${NTOTRANKS} --ppn ${NRANKS_PER_NODE} --depth=${NDEPTH} --cpu-bind core --env OMP_NUM_THREADS=${NTHREADS} -env OMP_PLACES=threads python train.py
+# Set up the PyTorch distributed environment
+export MASTER_ADDR=$(hostname)
+export MASTER_PORT=12345
+export WORLD_SIZE
+export RANK=$PBS_ARRAYID
+
+# Launch your script using torch.distributed.launch
+python -m torch.distributed.launch --nproc_per_node=$NUM_TASKS_PER_NODE train.py
