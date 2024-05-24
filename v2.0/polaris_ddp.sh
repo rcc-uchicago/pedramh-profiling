a4686a5511be162c5aab990d04e319b5721849ad
himanshisyadav <himanshimj@gmail.com>
2024-05-24 19:20:52 +0000

    Multi Node code edited

--- v2.0/polaris_ddp.sh
+++ v2.0/polaris_ddp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -l
 #PBS -N random
-#PBS -l select=1:system=polaris:ncpus=64:ngpus=4:gputype=A100
+#PBS -l select=1:system=polaris
 #PBS -l place=scatter
-#PBS -q debug 
-#PBS -l walltime=00:10:00
+#PBS -q prod 
+#PBS -l walltime=3:00:00
 #PBS -l filesystems=home:eagle                          
 #PBS -A lighthouse-uchicago
 #PBS -e logs/
@@ -12,13 +12,34 @@
 # Enable GPU-MPI (if supported by application)
 export MPICH_GPU_SUPPORT_ENABLED=1
 
+module use /soft/modulefiles
+ml conda
+conda activate /eagle/MDClimSim/hyadav/Pangu_env_2 
+ml cudatoolkit-standalone/12.2.2
+
+echo "Job ID: ${PBS_JOBID}"
+FINAL_MAX_EPOCHS=50
+
+EPOCHS_PER_JOB=2
+if [ -z "${MAX_EPOCHS}" ]; then MAX_EPOCHS=$EPOCHS_PER_JOB; fi
+
+MAX_EPOCHS_NEXT=$((MAX_EPOCHS + EPOCHS_PER_JOB))
+JOB_ID="${PBS_JOBID%%.*}"
+#JOB_ID=$PBS_JOBID
+echo "Max Epochs: ${MAX_EPOCHS}"
+echo "Max Epochs next: ${MAX_EPOCHS}"
+echo "Job ID: ${JOB_ID}"
+
+#if [ "$MAX_EPOCHS_NEXT" -le "$FINAL_MAX_EPOCHS" ]; then qsub -v MAX_EPOCHS="${MAX_EPOCHS_NEXT}" -W depend=afterok:$JOB_ID /eagle/MDClimSim/awikner/PanguWeather-UC/v2.0/polaris_ddp.sh; fi
+
 # Change to working directory
 cd $PBS_O_WORKDIR
 
 # MPI and OpenMP settings
 NNODES=`wc -l < $PBS_NODEFILE`
 #Follwing will be the number of GPUs on each node, so 4 in our case as each node has 4 GPUs
-NUM_TASKS_PER_NODE= $(nvidia-smi -L | wc -l)
+NUM_TASKS_PER_NODE=$(nvidia-smi -L | wc -l)
+#NUM_TASKS_PER_NODE=2
 WORLD_SIZE=$((NNODES * NUM_TASKS_PER_NODE))
 
 echo "NUM_OF_NODES= ${NNODES} NUM_TASKS_PER_NODE= ${NUM_TASKS_PER_NODE} WORLD_SIZE= ${WORLD_SIZE}"
@@ -30,4 +51,4 @@ export WORLD_SIZE
 export RANK=$PBS_ARRAYID
 
 # Launch your script using torch.distributed.launch
-python -m torch.distributed.launch --nproc_per_node=$NUM_TASKS_PER_NODE train.py
+python -m torch.distributed.launch --nproc_per_node=$NUM_TASKS_PER_NODE train.py --yaml_config=/eagle/MDClimSim/awikner/PanguWeather-UC/v2.0/config/PANGU_PLASIM_POLARIS.yaml --epochs=$MAX_EPOCHS
