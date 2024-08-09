Fixed error in input noise scaling

v2.0/utils/data_loader_multifiles.py
@@ -429,12 +429,12 @@ class GetDataset(Dataset):
             varying_boundary_data = self.boundary_transform(varying_boundary_data)
             if self.epsilon_factor > 0.:
                 if 'surface_ff_std' in self.params:
-                    surface_t_noise = torch.randn(*surface_t.shape) * (self.epsilon_factor * self.surface_std / self.surface_ff_std).reshape(len(self.surface_variables), 1, 1) * self. epsilon_factor
+                    surface_t_noise = torch.randn(*surface_t.shape) * (self.epsilon_factor * self.surface_ff_std / self.surface_std).reshape(len(self.surface_variables), 1, 1)
                 else:
                     surface_t_noise = torch.randn(*surface_t.shape) * self.epsilon_factor
                 surface_t = surface_t + surface_t_noise
                 if 'upper_air_ff_std' in self.params:
-                    upper_air_t_noise = torch.randn(*upper_air_t.shape) * (self.epsilon_factor * self.upper_air_std / self.upper_air_ff_std).reshape(len(self.upper_air_variables), self.num_levels, 1, 1)
+                    upper_air_t_noise = torch.randn(*upper_air_t.shape) * (self.epsilon_factor * self.upper_air_ff_std / self.upper_air_std).reshape(len(self.upper_air_variables), self.num_levels, 1, 1)
                 else:
                     upper_air_t_noise = torch.randn(*upper_air_t.shape) * self.epsilon_factor
                 upper_air_t = upper_air_t + upper_air_t_noise