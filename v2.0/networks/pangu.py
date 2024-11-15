Added option to not mask land or ocean in output and loss. Fixed bug in masking loss. Added option for convolutional head for surface, upper_air, and diagnostic variables.

v2.0/networks/pangu.py
@@ -128,12 +128,75 @@ class PanguModel_Plasim(nn.Module):
         if not drop_path:
             drop_path = np.append(np.linspace(0, 0.2, np.sum(params.depths[:2])), np.linspace(0.2, 0, np.sum(params.depths[2:]))).tolist()
 
+        # Masked output
+        if hasattr(params, 'mask_output'):
+            self.mask_output = params.mask_output
+        else:
+            self.mask_output = False
+        self.has_land = False
+        self.has_ocean = False
+        self.num_land_vars = 0
+        self.num_ocean_vars = 0
+        self.diagnostic_vars = []
+        self.num_diagnostic_vars = 0
+
+        if hasattr(params, 'land_variables'):
+            if len(params.land_variables) > 0:
+                self.has_land = True
+                self.num_land_vars = len(params.land_variables)
+                if not self.mask_output:
+                    print('mask_output is False. Land variables output will not be masked.')
+                else:
+                    if land_mask is None:
+                        lm_file        = os.path.join(params.data_dir, params.boundary_dir, 'lsm.nc')                
+                        land_mask_ds   = xr.open_dataset(lm_file)
+                        land_mask = torch.from_numpy(land_mask_ds.lsm.values).to(torch.float32)
+                        nans = torch.isnan(land_mask)
+                        land_mask = land_mask.masked_fill_(nans, 0.)
+                        land_mask_ds.close()
+                    if self.predict_delta:
+                        self.land_mask = Mask(land_mask)
+                    else:
+                        land_mask_fill = torch.stack([(1. - land_mask) * mask_fill[var] for var in params.land_variables])
+                        self.land_mask = Mask(land_mask, land_mask_fill)
+
+        if hasattr(params, 'ocean_variables'):
+            if len(params.ocean_variables) > 0:
+                self.has_ocean = True
+                self.num_ocean_vars = len(params.ocean_variables)
+                if not self.mask_output:
+                    print('mask_output is False. Ocean variables output will not be masked.')
+                else:
+                    if self.has_land:
+                        ocean_mask = (1. - land_mask)
+                    elif land_mask is None:
+                        lm_file        = os.path.join(params.data_dir, params.boundary_dir, 'lsm.nc')                
+                        land_mask_ds   = xr.open_dataset(lm_file)
+                        land_mask = torch.from_numpy(land_mask_ds.lsm.values).to(torch.float32)
+                        nans = torch.isnan(land_mask)
+                        land_mask = land_mask.masked_fill_(nans, 0.)
+                        land_mask_ds.close()
+                        ocean_mask = (1. - land_mask)
+                    if self.predict_delta:
+                        self.ocean_mask = Mask(ocean_mask)
+                    else:
+                        ocean_mask_fill = torch.stack([(1. - ocean_mask) * mask_fill[var] for var in params.ocean_variables])
+                        self.ocean_mask = Mask(ocean_mask, ocean_mask_fill)
+
+        if hasattr(params, 'diagnostic_variables'):
+            self.diagnostic_vars = params.diagnostic_variables
+            self.num_diagnostic_vars = len(self.diagnostic_vars)
+        print(f'Num diagnostic vars: {self.num_diagnostic_vars}')
+
         self.num_surface_vars = len(params.surface_variables)
         self.num_atmo_vars = len(params.upper_air_variables)
         self.num_boundary_vars = len(params.constant_boundary_variables) + len(params.varying_boundary_variables)
         self.atmo_resolution = [params.num_levels] + params.horizontal_resolution
         depths_cumsum = np.cumsum(params.depths).astype(int)
         self.predict_delta = params.predict_delta
+
+        self.surface_prognostic_idxs = torch.cat((torch.arange(self.num_surface_vars).long(), 
+                                                  torch.arange(self.num_surface_vars + self.num_diagnostic_vars, self.num_surface_vars + self.num_diagnostic_vars + self.num_land_vars + self.num_ocean_vars).long()))
         #if self.predict_delta:
         #    try:
         #        assert None not in [surface_ff_std, surface_delta_std, upper_air_ff_std, upper_air_delta_std]
@@ -145,6 +208,11 @@ class PanguModel_Plasim(nn.Module):
         self.vertical_windowing=params.vertical_windowing
         self.embed_dim = embed_dim
         self.updown_scale_factor = params.updown_scale_factor
+        self.subpixed_deconv = False
+        grid_has_poles = False
+        self.polar_pad = False
+        self.recovery_head = False
+        self.diagnostic_head = False
         if hasattr(params, 'subpixel_deconv'):
             self.subpixel_deconv = params.subpixel_deconv
             if hasattr(params, 'polar_pad'):
@@ -152,29 +220,17 @@ class PanguModel_Plasim(nn.Module):
                 if self.polar_pad and hasattr(params, 'grid_has_poles'):
                     grid_has_poles = params.grid_has_poles
                 elif self.polar_pad:
-                    print('Polar padding for patch recovery is enables, but grid_has_poles is unspecified. If grid has poles, this will lead to patch artifacts.')
-                    grid_has_poles = False
-                else:
-                    grid_has_poles = False
-
-            else:
-                self.polar_pad = False
-                grid_has_poles = False
-        else:
-            self.subpixel_deconv = False
-            grid_has_poles = False
+                    print('Polar padding for patch recovery is enabled, but grid_has_poles is unspecified. If grid has poles, this will lead to patch artifacts.')
+            if hasattr(params, 'recovery_head'):
+                self.recovery_head = params.recovery_head
+            if hasattr(params, 'diagnostic_head'):
+                self.diagnostic_head = params.diagnostic_head
         
         
 
         self.upper_air_boundary = params.upper_air_boundary
         self.varying_boundary_variables = params.varying_boundary_variables
         self.num_varying_boundary_vars = len(params.varying_boundary_variables)
-        try:
-            self.diagnostic_vars = params.diagnostic_variables
-        except:
-            self.diagnostic_vars = []
-        self.num_diagnostic_vars = len(self.diagnostic_vars)
-        print(f'Num diagnostic vars: {self.num_diagnostic_vars}')
         self.idx_upper_air_var_bound= self.varying_boundary_variables.index('rsdt') # careful, if change, change also the self.patchembed2d_upper_air_boundary and patchembed2d
         self.idx_surface_var_bound = [i for i in range(self.num_varying_boundary_vars) if i != self.idx_upper_air_var_bound]
     
@@ -193,7 +249,7 @@ class PanguModel_Plasim(nn.Module):
         self.patchembed2d = PatchEmbed2D(
             img_size=params.horizontal_resolution,
             patch_size=params.patch_size[1:],
-            in_chans=self.num_surface_vars + self.num_boundary_vars - 1*self.upper_air_boundary,
+            in_chans=self.num_surface_vars + self.num_land_vars + self.num_ocean_vars + self.num_boundary_vars - 1*self.upper_air_boundary,
             embed_dim=embed_dim,)
             
         
@@ -273,65 +329,39 @@ class PanguModel_Plasim(nn.Module):
             drop_path=drop_path[depths_cumsum[2]:],
             vertical_windowing=params.vertical_windowing)
         
+        
         # The outputs of the 2nd encoder layer and the 7th decoder layer are concatenated along the channel dimension.
         
         if self.subpixel_deconv:
-            from utils.patch_recovery import SubPixelConvICNR_2D, SubPixelConvICNR_3D
-            self.patchrecovery2d = SubPixelConvICNR_2D(params.horizontal_resolution, params.patch_size[1:], 2 * embed_dim, self.num_surface_vars + self.num_diagnostic_vars,
-                                                       num_lat = self.atmo_resolution[1], polar_pad=self.polar_pad, grid_has_poles = grid_has_poles)
-            self.patchrecovery3d = SubPixelConvICNR_3D(self.atmo_resolution, params.patch_size, 2 * embed_dim, self.num_atmo_vars, padded_front = self.patchembed3d.padded_front,
-                                                       num_lat = self.atmo_resolution[1], polar_pad=self.polar_pad, grid_has_poles = grid_has_poles)
+            if self.recovery_head:
+                from utils.patch_recovery import SubPixelConvICNR_2D_wHead as SubPixelConv_2D
+                from utils.patch_recovery import SubPixelConvICNR_3D_wHead as SubPixelConv_3D
+
+                if self.diagnostic_head:
+                    self.patchrecovery2d = SubPixelConv_2D(params.horizontal_resolution, params.patch_size[1:], 2 * embed_dim, 
+                                                           self.num_surface_vars, diagnostic_variables=self.num_diagnostic_vars, diagnostic_head=self.diagnostic_head,
+                                                           land_variables=self.num_land_vars, ocean_variables=self.num_ocean_vars,
+                                                           num_lat = self.atmo_resolution[1], polar_pad=self.polar_pad, grid_has_poles = grid_has_poles)
+                else:
+                    self.patchrecovery2d = SubPixelConv_2D(params.horizontal_resolution, params.patch_size[1:], 2 * embed_dim, 
+                                                           self.num_surface_vars + self.num_diagnostic_vars, diagnostic_variables=0, diagnostic_head=self.diagnostic_head,
+                                                           land_variables=self.num_land_vars, ocean_variables=self.num_ocean_vars,
+                                                           num_lat = self.atmo_resolution[1], polar_pad=self.polar_pad, grid_has_poles = grid_has_poles)
+                
+                self.patchrecovery3d = SubPixelConv_3D(self.atmo_resolution, params.patch_size, 2 * embed_dim, self.num_atmo_vars, padded_front = self.patchembed3d.padded_front,
+                                                        num_lat = self.atmo_resolution[1], polar_pad=self.polar_pad, grid_has_poles = grid_has_poles)
+            else:
+                from utils.patch_recovery import SubPixelConvICNR_2D as SubPixelConv_2D
+                from utils.patch_recovery import SubPixelConvICNR_3D as SubPixelConv_3D
+                self.patchrecovery2d = SubPixelConv_2D(params.horizontal_resolution, params.patch_size[1:], 2 * embed_dim, 
+                                                    self.num_surface_vars + self.num_diagnostic_vars + self.num_land_vars + self.num_ocean_vars,
+                                                        num_lat = self.atmo_resolution[1], polar_pad=self.polar_pad, grid_has_poles = grid_has_poles)
+                self.patchrecovery3d = SubPixelConv_3D(self.atmo_resolution, params.patch_size, 2 * embed_dim, self.num_atmo_vars, padded_front = self.patchembed3d.padded_front,
+                                                        num_lat = self.atmo_resolution[1], polar_pad=self.polar_pad, grid_has_poles = grid_has_poles)
             
         else:
-            self.patchrecovery2d = PatchRecovery2D(params.horizontal_resolution, params.patch_size[1:], 2 * embed_dim, self.num_surface_vars + self.num_diagnostic_vars)
+            self.patchrecovery2d = PatchRecovery2D(params.horizontal_resolution, params.patch_size[1:], 2 * embed_dim, self.num_surface_vars + self.num_diagnostic_vars + self.num_land_vars + self.num_ocean_vars)
             self.patchrecovery3d = PatchRecovery3D(self.atmo_resolution, params.patch_size, 2 * embed_dim, self.num_atmo_vars)
-        
-        # Masked output
-
-        if hasattr(params, 'land_variables'):
-            if len(params.land_variables) > 0:
-                self.has_land = True
-                if land_mask is None:
-                    lm_file        = os.path.join(params.data_dir, params.boundary_dir, 'lsm.nc')                
-                    land_mask_ds   = xr.open_dataset(lm_file)
-                    land_mask = torch.from_numpy(land_mask_ds.lsm.values).to(torch.float32)
-                    nans = torch.isnan(land_mask)
-                    land_mask = land_mask.masked_fill_(nans, 0.)
-                    land_mask_ds.close()
-                if self.predict_delta:
-                    self.land_mask = Mask(land_mask)
-                else:
-                    land_mask_fill = torch.stack([(1. - land_mask) * mask_fill[var] for var in params.land_variables])
-                    self.land_mask = Mask(land_mask, land_mask_fill)
-                self.land_idxs = [var in params.land_variables for var in params.surface_variables]
-            else:
-                self.has_land = False
-        else:
-            self.has_land = False
-
-        if hasattr(params, 'ocean_variables'):
-            if len(params.ocean_variables) > 0:
-                self.has_ocean = True
-                if self.has_land:
-                    ocean_mask = (1. - land_mask)
-                elif land_mask is None:
-                    lm_file        = os.path.join(params.data_dir, params.boundary_dir, 'lsm.nc')                
-                    land_mask_ds   = xr.open_dataset(lm_file)
-                    land_mask = torch.from_numpy(land_mask_ds.lsm.values).to(torch.float32)
-                    nans = torch.isnan(land_mask)
-                    land_mask = land_mask.masked_fill_(nans, 0.)
-                    land_mask_ds.close()
-                    ocean_mask = (1. - land_mask)
-                if self.predict_delta:
-                    self.ocean_mask = Mask(ocean_mask)
-                else:
-                    ocean_mask_fill = torch.stack([(1. - ocean_mask) * mask_fill[var] for var in params.ocean_variables])
-                    self.ocean_mask = Mask(ocean_mask, ocean_mask_fill)
-                self.ocean_idxs = [var in params.ocean_variables for var in params.surface_variables]
-            else:
-                self.has_ocean = False
-        else:
-            self.has_ocean = False
 
 
     def forward(self, surface_in, constant_boundary, varying_boundary, upper_air_in):
@@ -469,11 +499,13 @@ class PanguModel_Plasim(nn.Module):
             else:
                 output_upper_air_delta = output[:, :, :-1, :, :]
             output_2D = self.patchrecovery2d(output_surface_delta)
-            output_surface = output_2D[:, :self.num_surface_vars]
-            if self.has_land:
-                output_surface[:, self.land_idxs] = self.land_mask(output_surface[:, self.land_idxs]).to(output_surface.dtype)
-            if self.has_ocean:
-                output_surface[:, self.ocean_idxs] = self.ocean_mask(output_surface[:, self.ocean_idxs]).to(output_surface.dtype)
+            output_surface = output_2D[:, self.surface_prognostic_idxs]
+            if self.has_land and self.mask_output:
+                output_surface[:, self.num_surface_vars: self.num_surface_vars + self.num_land_vars] = \
+                    self.land_mask(output_surface[:, self.num_surface_vars: self.num_surface_vars + self.num_land_vars]).to(output_surface.dtype)
+            if self.has_ocean and self.mask_output:
+                output_surface[:, self.num_surface_vars + self.num_land_vars:] = \
+                    self.land_mask(output_surface[:, self.num_surface_vars + self.num_land_vars:]).to(output_surface.dtype)
             output_upper_air = self.patchrecovery3d(output_upper_air_delta)
         else:
             output_surface = output[:, :, -1, :, :]
@@ -481,13 +513,14 @@ class PanguModel_Plasim(nn.Module):
                 output_upper_air = output[:, :, 1:-1, :, :]
             else:
                 output_upper_air = output[:, :, :-1, :, :]
-
             output_2D = self.patchrecovery2d(output_surface)
-            output_surface = output_2D[:, :self.num_surface_vars]
-            if self.has_land:
-                output_surface[:, self.land_idxs] = self.land_mask(output_surface[:, self.land_idxs]).to(output_surface.dtype)
-            if self.has_ocean:
-                output_surface[:, self.ocean_idxs] = self.ocean_mask(output_surface[:, self.ocean_idxs]).to(output_surface.dtype)
+            output_surface = output_2D[:, self.surface_prognostic_idxs]
+            if self.has_land and self.mask_output:
+                output_surface[:, self.num_surface_vars : self.num_surface_vars + self.num_land_vars] = \
+                    self.land_mask(output_surface[:, self.num_surface_vars: self.num_surface_vars + self.num_land_vars]).to(output_surface.dtype)
+            if self.has_ocean and self.mask_output:
+                output_surface[:, self.num_surface_vars + self.num_land_vars:] = \
+                    self.land_mask(output_surface[:, self.num_surface_vars + self.num_land_vars:]).to(output_surface.dtype)
             #print(f'Shape after 2D patch recovery:{x.shape}')
             #print(output_2D[0,0,:,0])
             #print(output_2D[0,0,:,-1])
@@ -500,7 +533,7 @@ class PanguModel_Plasim(nn.Module):
             #print(torch.std(output_upper_air, dim=(0,1,4)))
             #print(torch.std(output_upper_air))
         if self.num_diagnostic_vars > 0:
-            output_diagnostic = output_2D[:, self.num_surface_vars:].reshape(
+            output_diagnostic = output_2D[:, self.num_surface_vars:self.num_surface_vars + self.num_diagnostic_vars].reshape(
                 output_surface.shape[0], -1, output_surface.shape[-2], output_surface.shape[-1])
             return output_surface, output_upper_air, output_diagnostic
         else:

v2.0/train.py
@@ -223,6 +223,7 @@ class Trainer():
         self.run_uuid = str(uuid.uuid4())
         self.has_land = False
         self.has_ocean = False
+        self.mask_output = False
         if hasattr(self.params, 'land_variables'):
             if len(self.params.land_variables) > 0:
                 self.has_land = True
@@ -233,6 +234,8 @@ class Trainer():
                 self.has_land = True
         else:
             self.params['ocean_variables'] = []
+        if hasattr(self.params, 'mask_output'):
+            self.mask_output = params.mask_output
 
 
         logging.info('rank %d, begin data loader init' % world_rank)
@@ -331,8 +334,8 @@ class Trainer():
                 resume = "never"
             wandb.init(config=params, name=params.name, group=params.group, project=params.project, resume=resume)#, entity=params.entity)
 
-            wandb.define_metric("custom_step")
-            wandb.define_metric("power_spectrum_plot", step_metric="custom_step")
+            #wandb.define_metric("custom_step")
+            #wandb.define_metric("power_spectrum_plot", step_metric="custom_step")
 
 
         
@@ -341,6 +344,7 @@ class Trainer():
             #           entity=params.entity)
             wandb.define_metric("epoch")
             wandb.define_metric("ACC_plot", step_metric="epoch")
+            wandb.define_metric("power_spectrum_plot", step_metric="epoch")
             if self.params.diagnostic_logs:
                 epoch_metrics = ['lr', 'train_loss', 'valid_loss', 'valid_loss_sfc', 'valid_loss_upper_air', 'valid_mean_norm_lwrmse']
                 for l, steps in enumerate(self.params.forecast_lead_times):
@@ -363,7 +367,7 @@ class Trainer():
 
 
         if params.nettype == 'pangu_plasim':
-            if self.has_land or self.has_ocean:
+            if (self.has_land or self.has_ocean) and self.mask_output:
                 land_mask = torch.clone(self.train_datasets[0].land_mask.detach()).to(self.device)
                 print(f'Land Mask shape: {land_mask.shape}')
                 mask_bool = []
@@ -473,7 +477,7 @@ class Trainer():
             logging.info("Number of trainable model parameters: {}".format(self.count_parameters()))
         if params.loss == 'l1':
             self.loss_obj_pl = torch.nn.L1Loss()
-            if self.has_land or self.has_ocean:
+            if (self.has_land or self.has_ocean) and self.mask_output:
                 self.loss_obj_sfc = Masked_L1Loss(mask_bool)
             else:
                 self.loss_obj_sfc = torch.nn.L1Loss()
@@ -481,7 +485,7 @@ class Trainer():
                 self.loss_obj_diagnostic = torch.nn.L1Loss()
         elif params.loss == 'l2':
             self.loss_obj_pl = torch.nn.MSELoss()
-            if self.has_land or self.has_ocean:
+            if (self.has_land or self.has_ocean) and self.mask_output:
                 self.loss_obj_sfc = Masked_MSELoss(mask_bool)
             else:
                 self.loss_obj_sfc = torch.nn.MSELoss()
@@ -492,7 +496,7 @@ class Trainer():
 
             # self.lat = self.train_dataset.lat.to(self.device, non_blocking=True)
             self.loss_obj_pl = Latitude_weighted_L1Loss(self.lat)
-            if self.has_land or self.has_ocean:
+            if (self.has_land or self.has_ocean) and self.mask_output:
                 self.loss_obj_sfc = Latitude_weighted_masked_L1Loss(self.lat, mask_bool)
             else:
                 self.loss_obj_sfc = Latitude_weighted_L1Loss(self.lat)
@@ -502,7 +506,7 @@ class Trainer():
             self.lat = self.train_datasets[0].lat.to(self.device)
             
             self.loss_obj_pl = Latitude_weighted_MSELoss(self.lat)
-            if self.has_land or self.has_ocean:
+            if (self.has_land or self.has_ocean) and self.mask_output:
                 self.loss_obj_sfc = Latitude_weighted_masked_MSELoss(self.lat, mask_bool)
             else:
                 self.loss_obj_sfc = Latitude_weighted_MSELoss(self.lat)
@@ -587,12 +591,13 @@ class Trainer():
                 break # Exit the train method
             
         # After the training loop ends
-        if self.params.log_to_wandb:
-            if self.world_rank == 0:
-                self.log_all_plots_to_wandb()
+        #if self.params.log_to_wandb:
+        #    if self.world_rank == 0:
+        #        self.log_all_plots_to_wandb()
         if self.world_rank == 0:
             self.cleanup_acc_plots()
             self.cleanup_gifs()
+            self.cleanup_power_spectrum_plots()
         # If we've reached this point, we've completed all epochs
         if self.params.log_to_screen:
             if early_stop_epoch_triggered:
@@ -1124,6 +1129,12 @@ class Trainer():
             shutil.rmtree(output_dir)
             print(f"Deleted ACC plots directory: {output_dir}")
 
+    def cleanup_power_spectrum_plots(self):
+        output_dir = os.path.join(os.getcwd(), "spectra_out", self.run_uuid)
+        if os.path.exists(output_dir):
+            shutil.rmtree(output_dir)
+            print(f"Deleted Power Spectrum plots directory: {output_dir}")
+
     def cleanup_gifs(self):
         if os.path.exists(self.diagnostics_dir):
             shutil.rmtree(self.diagnostics_dir)
@@ -1506,14 +1517,18 @@ class Trainer():
             if self.params.log_to_wandb:
                 wandb.log(diagnostic_logs)
                 wandb.log({
-                "ACC_plot": wandb.Image(plot_filename),
-                "epoch": self.epoch
+                    "ACC_plot": wandb.Image(plot_filename),
+                    "epoch": self.epoch
                 })
                 if gif_filename:
                     wandb.log({
-                "Evolution_GIF": wandb.Video(gif_filename),
-                "epoch": self.epoch
-            })
+                        "Evolution_GIF": wandb.Video(gif_filename),
+                        "epoch": self.epoch
+                    })
+                wandb.log({
+                    "power_spectrum_plot": wandb.Image(path_filename),
+                    "epoch": self.epoch,
+                })
             
             valid_time = time.time() - valid_start
 
@@ -1523,7 +1538,7 @@ class Trainer():
                 if self.params.has_diagnostic:
                     logs = {'valid_loss': valid_buff_cpu[0], 
                             'valid_loss_sfc': valid_buff_cpu[1], 'valid_loss_upper_air': valid_buff_cpu[2],
-                            'valid_loss_diag': valud_buff_cpu[3],
+                            'valid_loss_diag': valid_buff_cpu[3],
                             'epoch': self.epoch}
                 else:
                     logs = {'valid_loss': valid_buff_cpu[0], 
@@ -1546,9 +1561,13 @@ class Trainer():
                 })
                 if gif_filename:
                     wandb.log({
-                "Evolution_GIF": wandb.Video(gif_filename),
-                "epoch": self.epoch
-            })
+                        "Evolution_GIF": wandb.Video(gif_filename),
+                        "epoch": self.epoch
+                    })
+                wandb.log({
+                    "power_spectrum_plot": wandb.Image(path_filename),
+                    "epoch": self.epoch,
+                })
 
             valid_time = time.time() - valid_start
 
@@ -1588,8 +1607,8 @@ class Trainer():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--run_num", default='0173', type=str)
-    parser.add_argument("--yaml_config", default='v2.0/config/PANGU_NEW_0173.yaml', type=str)
+    parser.add_argument("--run_num", default='0188', type=str)
+    parser.add_argument("--yaml_config", default='v2.0/config/PANGU_NEW_0188.yaml', type=str)
     parser.add_argument("--config", default='PLASIM', type=str)
     parser.add_argument("--enable_amp", default=True, action='store_true')
     parser.add_argument("--epsilon_factor", default=0, type=float)

v2.0/utils/data_loader_multifiles.py
@@ -124,6 +124,16 @@ class GetDataset(Dataset):
         # self.timedelta = self.datetime_class(1, 1, 1, hour=self.timedelta_hours) - self.datetime_class(1, 1, 1, hour=0)
 
         self.surface_variables = params.surface_variables or []
+        if hasattr(params, 'land_variables'):
+            if any([land_variable in self.surface_variables for land_variable in params.land_variables]):
+                raise ValueError('land variables cannot be in surface variables.')
+            self.land_variables = params.land_variables
+            self.surface_variables = self.surface_variables + self.land_variables
+        if hasattr(params, 'ocean_variables'):
+            if any([ocean_variable in self.surface_variables for ocean_variable in params.ocean_variables]):
+                raise ValueError('ocean variables cannot be in surface variables.')
+            self.ocean_variables = params.ocean_variables
+            self.surface_variables = self.surface_variables + self.ocean_variables
         self.upper_air_variables = params.upper_air_variables or []
 
         self.constant_boundary_variables = params.constant_boundary_variables or []
@@ -453,7 +463,7 @@ class GetDataset(Dataset):
         #print(data_ds['tas'].time)
         surface_da_list = []
         for var in self.surface_variables:
-            if var in self.land_variables:
+            if var in self.land_variables or var in self.ocean_variables:
                 var_data = torch.from_numpy(data_ds[var].sel(time=hour).values).to(torch.float32)
                 nans = torch.isnan(var_data)
                 if torch.any(nans):

v2.0/utils/patch_recovery.py
@@ -131,6 +131,164 @@ class SubPixelConvICNR_2D(nn.Module):
         padding_right = int(w_pad - padding_left)
 
         return output[:, :, padding_top: H - padding_bottom, padding_left: W - padding_right]
+    
+class SubPixelConvICNR_2D_wHead(nn.Module):
+    """
+    Patch Embedding Recovery to 2D Image.
+
+    Args:
+        img_size (tuple[int]): Lat, Lon
+        patch_size (tuple[int]): Lat, Lon
+        in_chans (int): Number of input channels.
+        out_chans (int): Number of output channels.
+    """
+
+    def __init__(self, img_size, patch_size, in_chans, out_chans, diagnostic_variables = 0, diagnostic_head = True, land_variables = 0, 
+                 ocean_variables = 0, num_lat = None, polar_pad = True, grid_has_poles = False, hidden_dim = 96):
+        super().__init__()
+        self.img_size = img_size
+        self.diagnostic_head = diagnostic_head
+        self.diagnostic_variables = diagnostic_variables
+        self.land_variables = land_variables
+        self.ocean_variables = ocean_variables
+        assert patch_size[0] == patch_size[1], 'mismatch'
+        if polar_pad:
+            self.pad_poles = PolarPad2d((1, 1), num_lat=num_lat, grid_has_poles=grid_has_poles)
+            self.head = nn.Sequential(
+                PolarPad2d((1, 1), num_lat = num_lat, grid_has_poles=grid_has_poles),
+                nn.CircularPad2d((1, 1, 0, 0)),
+                nn.Conv2d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
+                nn.GELU(),
+                PolarPad2d((1, 1), num_lat = num_lat, grid_has_poles=grid_has_poles),
+                nn.CircularPad2d((1, 1, 0, 0)),
+                nn.Conv2d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
+                nn.GELU(),
+                nn.Conv2d(hidden_dim, out_chans, kernel_size=1, stride=1, padding=0)
+            )
+            if diagnostic_variables > 0 and diagnostic_head:
+                self.diagnostic_head = nn.Sequential(
+                    PolarPad2d((1, 1), num_lat = num_lat, grid_has_poles=grid_has_poles),
+                    nn.CircularPad2d((1, 1, 0, 0)),
+                    nn.Conv2d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
+                    nn.GELU(),
+                    PolarPad2d((1, 1), num_lat = num_lat, grid_has_poles=grid_has_poles),
+                    nn.CircularPad2d((1, 1, 0, 0)),
+                    nn.Conv2d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
+                    nn.GELU(),
+                    nn.Conv2d(hidden_dim, diagnostic_variables, kernel_size=1, stride=1, padding=0)
+                )
+            if land_variables > 0:
+                self.land_head = nn.Sequential(
+                    PolarPad2d((1, 1), num_lat = num_lat, grid_has_poles=grid_has_poles),
+                    nn.CircularPad2d((1, 1, 0, 0)),
+                    nn.Conv2d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
+                    nn.GELU(),
+                    PolarPad2d((1, 1), num_lat = num_lat, grid_has_poles=grid_has_poles),
+                    nn.CircularPad2d((1, 1, 0, 0)),
+                    nn.Conv2d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
+                    nn.GELU(),
+                    nn.Conv2d(hidden_dim, self.land_variables, kernel_size=1, stride=1, padding=0)
+                )
+            if ocean_variables > 0:
+                self.ocean_head = nn.Sequential(
+                    PolarPad2d((1, 1), num_lat = num_lat, grid_has_poles=grid_has_poles),
+                    nn.CircularPad2d((1, 1, 0, 0)),
+                    nn.Conv2d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
+                    nn.GELU(),
+                    PolarPad2d((1, 1), num_lat = num_lat, grid_has_poles=grid_has_poles),
+                    nn.CircularPad2d((1, 1, 0, 0)),
+                    nn.Conv2d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
+                    nn.GELU(),
+                    nn.Conv2d(hidden_dim, self.ocean_variables, kernel_size=1, stride=1, padding=0)
+                )
+        else:
+            self.pad_poles = nn.ZeroPad2d((0, 0, 1, 1))
+            self.head = nn.Sequential(
+                nn.ZeroPad2d((0, 0, 1, 1)),
+                nn.CircularPad2d((1, 1, 0, 0)),
+                nn.Conv2d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
+                nn.GELU(),
+                nn.ZeroPad2d((0, 0, 1, 1)),
+                nn.CircularPad2d((1, 1, 0, 0)),
+                nn.Conv2d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
+                nn.GELU(),
+                nn.Conv2d(hidden_dim, out_chans, kernel_size=1, stride=1, padding=0)
+            )
+            if diagnostic_variables > 0 and diagnostic_head:
+                self.diagnostic_head = nn.Sequential(
+                    nn.ZeroPad2d((0, 0, 1, 1)),
+                    nn.CircularPad2d((1, 1, 0, 0)),
+                    nn.Conv2d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
+                    nn.GELU(),
+                    nn.ZeroPad2d((0, 0, 1, 1)),
+                    nn.CircularPad2d((1, 1, 0, 0)),
+                    nn.Conv2d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
+                    nn.GELU(),
+                    nn.Conv2d(hidden_dim, diagnostic_variables, kernel_size=1, stride=1, padding=0)
+                )
+            if self.land_variables > 0:
+                self.land_head = nn.Sequential(
+                    nn.ZeroPad2d((0, 0, 1, 1)),
+                    nn.CircularPad2d((1, 1, 0, 0)),
+                    nn.Conv2d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
+                    nn.GELU(),
+                    nn.ZeroPad2d((0, 0, 1, 1)),
+                    nn.CircularPad2d((1, 1, 0, 0)),
+                    nn.Conv2d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
+                    nn.GELU(),
+                    nn.Conv2d(hidden_dim, self.land_variables, kernel_size=1, stride=1, padding=0)
+                )
+            if self.ocean_variables > 0:
+                self.ocean_head = nn.Sequential(
+                    nn.ZeroPad2d((0, 0, 1, 1)),
+                    nn.CircularPad2d((1, 1, 0, 0)),
+                    nn.Conv2d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
+                    nn.GELU(),
+                    nn.ZeroPad2d((0, 0, 1, 1)),
+                    nn.CircularPad2d((1, 1, 0, 0)),
+                    nn.Conv2d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
+                    nn.GELU(),
+                    nn.Conv2d(hidden_dim, self.ocean_variables, kernel_size=1, stride=1, padding=0)
+                )
+
+        self.pad_circular = nn.CircularPad2d((1, 1, 0, 0))
+        self.conv = nn.Conv2d(in_chans, hidden_dim*patch_size[0]**2, kernel_size=3, stride=1, padding=0, bias=0)
+        self.pixelshuffle = nn.PixelShuffle(patch_size[0])
+        weight = ICNR(self.conv.weight, 
+                      initializer=nn.init.kaiming_normal_,
+                      upscale_factor=patch_size[0])
+        self.conv.weight.data.copy_(weight)   # initialize conv.weight
+
+    def forward(self, x):
+        x_padded = self.pad_poles(self.pad_circular(x))
+        x_upsample = self.conv(x_padded)
+        #print(output.shape)
+        
+        x_upsample = self.pixelshuffle(x_upsample)
+        #print(output.shape)
+        
+        _, _, H, W = x_upsample.shape
+        h_pad = H - self.img_size[0]
+        w_pad = W - self.img_size[1]
+
+        padding_top = h_pad // 2
+        padding_bottom = int(h_pad - padding_top)
+
+        padding_left = w_pad // 2
+        padding_right = int(w_pad - padding_left)
+
+        x_upsample = x_upsample[:, :, padding_top: H - padding_bottom, padding_left: W - padding_right]
+        output = self.head(x_upsample)
+        if self.diagnostic_variables > 0 and self.diagnostic_head:
+            diagnostic_output = self.diagnostic_head(x_upsample)
+            output = torch.cat([output, diagnostic_output], dim = 1)
+        if self.land_variables > 0:
+            land_output = self.land_head(x_upsample)
+            output = torch.cat([output, land_output], dim = 1)
+        if self.ocean_variables > 0:
+            ocean_output = self.ocean_head(x_upsample)
+            output = torch.cat([output, ocean_output], dim = 1)
+        return output
 
 
 class SubPixelConvICNR_3D(nn.Module):
@@ -195,27 +353,171 @@ class SubPixelConvICNR_3D(nn.Module):
         return output[:, :, padding_front: Pl - padding_back,
                padding_top: Lat - padding_bottom, padding_left: Lon - padding_right]
     
+
+class SubPixelConvICNR_3D_wHead(nn.Module):
+    """
+    Patch Embedding Recovery to 3D Image.
+
+    Args:
+        img_size (tuple[int]): Pl, Lat, Lon
+        patch_size (tuple[int]): Pl, Lat, Lon
+        in_chans (int): Number of input channels.
+        out_chans (int): Number of output channels.
+    """
+
+    def __init__(self, img_size, patch_size, in_chans, out_chans, padded_front = False, polar_pad = True, num_lat = None,
+                 grid_has_poles = False, hidden_dim = 96):
+        super().__init__()
+        self.img_size = img_size
+        self.padded_front = padded_front
+        assert patch_size[1] == patch_size[2], 'mismatch'
+        if polar_pad:
+            self.pad_poles = PolarPad3d((1, 1), num_lat = num_lat, grid_has_poles=grid_has_poles)
+            self.head = nn.Sequential(
+                PolarPad3d((1, 1), num_lat = num_lat, grid_has_poles=grid_has_poles),
+                nn.CircularPad3d((1, 1, 0, 0, 0, 0)),
+                nn.ZeroPad3d((0, 0, 0, 0, 1, 1)),
+                nn.Conv3d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
+                nn.GELU(),
+                PolarPad3d((1, 1), num_lat = num_lat, grid_has_poles=grid_has_poles),
+                nn.CircularPad3d((1, 1, 0, 0, 0, 0)),
+                nn.ZeroPad3d((0, 0, 0, 0, 1, 1)),
+                nn.Conv3d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
+                nn.GELU(),
+                nn.Conv3d(hidden_dim, out_chans, kernel_size=1, stride=1, padding=0)
+            )
+        else:
+            self.pad_poles = nn.ZeroPad3d((0, 0, 1, 1, 0, 0))
+            self.head = nn.Sequential(
+                nn.ZeroPad3d((0, 0, 1, 1, 1, 1)),
+                nn.CircularPad3d((1, 1, 0, 0, 0, 0)),
+                nn.Conv3d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
+                nn.GELU(),
+                nn.ZeroPad3d((0, 0, 1, 1, 1, 1)),
+                nn.CircularPad3d((1, 1, 0, 0, 0, 0)),
+                nn.Conv3d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
+                nn.GELU(),
+                nn.Conv3d(hidden_dim, out_chans, kernel_size=1, stride=1, padding=0)
+            )
+        self.pad_circular = nn.CircularPad3d((1, 1, 0, 0, 0, 0))
+        self.conv = nn.Conv2d(in_chans//2, hidden_dim*patch_size[1]**2, kernel_size=3, stride=1, padding=0, bias=0)
+        self.pixelshuffle = nn.PixelShuffle(patch_size[1])
+        weight = ICNR(self.conv.weight, 
+                      initializer=nn.init.kaiming_normal_,
+                      upscale_factor=patch_size[1])
+        self.conv.weight.data.copy_(weight)   # initialize conv.weight
+
+    def forward(self, x: torch.Tensor):
+        # first, split in dimension
+        # print(x.shape)
+        x_padded = self.pad_poles(self.pad_circular(x))
+        x_padded = x_padded.reshape(x_padded.shape[0], x_padded.shape[1]//2, 2, *x_padded.shape[2:])
+        x_padded = x_padded.flatten(2, 3)
+        if not self.padded_front:
+            x_padded = x_padded[:, :, 0:self.img_size[0]]
+        else:
+            x_padded = x_padded[:, :, 1:self.img_size[0]+1] # to make 13 vertical dims
+        x_padded = x_padded.movedim(-3, 1).flatten(0, 1)
+        x_upsample = self.conv(x_padded)
+        x_upsample = self.pixelshuffle(x_upsample)
+        x_upsample = x_upsample.reshape(-1, self.img_size[0], *x_upsample.shape[1:]).movedim(1, -3)
+        
+        _, _, Pl, Lat, Lon = x_upsample.shape
+
+        pl_pad = Pl - self.img_size[0]
+        lat_pad = Lat - self.img_size[1]
+        lon_pad = Lon - self.img_size[2]
+
+        padding_front = pl_pad // 2
+        padding_back = pl_pad - padding_front
+
+        padding_top = lat_pad // 2
+        padding_bottom = lat_pad - padding_top
+
+        padding_left = lon_pad // 2
+        padding_right = lon_pad - padding_left
+
+        x_upsample = x_upsample[:, :, padding_front: Pl - padding_back,
+               padding_top: Lat - padding_bottom, padding_left: Lon - padding_right]
+        
+        output = self.head(x_upsample)
+        return output
+
+
+        
+
+    
 class PatchRecovery5(nn.Module):
     ''' true upsampling with 3D conv
     '''
-    def __init__(self, 
-                 input_dim=None,
-                 dim=192,
-                 downfactor=4,
-                 hidden_dim=96,
-                 output_dim=69,
-                 n_level_variables=5):
+    def __init__(self, img_size, patch_size, num_levels, in_chans, out_chans, hidden_dim = 96, 
+                 padded_front = False, polar_pad = True, num_lat = None,
+                 grid_has_poles = False, downfactor = 4):
+        #def __init__(self, 
+        #             input_dim=None,
+        #             dim=192,
+        #             downfactor=4,
+        #             hidden_dim=96,
+        #             output_dim=69,
+        #             n_level_variables=5):
         # input dim equals input_dim*z since we will be flattening stuff ?
         super().__init__()
         self.downfactor = downfactor
-        if input_dim is None:
-            input_dim = 8*dim
-        
-        self.input_conv = nn.Conv2d(input_dim, 14*hidden_dim, kernel_size=1, stride=1, padding=0)
+        self.num_levels = num_levels
+        #if input_dim is None:
+        #    input_dim = 8*dim
+
+        self.img_size = img_size
+        self.padded_front = padded_front
+        assert patch_size[1] == patch_size[2], 'mismatch'
+        if polar_pad:
+            self.conv = nn.Sequential(
+                PolarPad2d((1, 1), num_lat = num_lat, grid_has_poles=grid_has_poles),
+                nn.CircularPad3d((1, 1, 0, 0)),
+                nn.Conv2d(in_chans//2, out_chans*patch_size[1]**2, kernel_size=3, stride=1, padding=0, bias=0),
+                nn.PixelShuffle(patch_size[1])
+            )
+            nn.Conv2d(in_chans, num_levels*hidden_dim, kernel_size=1, stride=1, padding=0)
+            self.head = nn.Sequential(
+                PolarPad3d((1, 1), num_lat = num_lat, grid_has_poles=grid_has_poles),
+                nn.CircularPad3d((1, 1, 0, 0, 0, 0)),
+                nn.Conv3d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
+                nn.GELU(),
+                PolarPad3d((1, 1), num_lat = num_lat, grid_has_poles=grid_has_poles),
+                nn.CircularPad3d((1, 1, 0, 0, 0, 0)),
+                nn.Conv3d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
+                nn.GELU()
+            )
+            if downfactor == 4:
+                self.head2 = nn.Sequential(
+                nn.GroupNorm(num_groups=32, num_channels=hidden_dim, eps=1e-6, affine=True),
+                PolarPad3d((1, 1), num_lat = num_lat, grid_has_poles=grid_has_poles),
+                nn.CircularPad3d((1, 1, 0, 0, 0, 0)),
+                nn.Conv3d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
+                nn.GELU(),
+                PolarPad3d((1, 1), num_lat = num_lat, grid_has_poles=grid_has_poles),
+                nn.CircularPad3d((1, 1, 0, 0, 0, 0)),
+                nn.Conv3d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0), # kernel size 3 for interactions and smoothing
+                nn.GELU())
+            pad_poles_2D = PolarPad2d((1, 1), num_lat = num_lat, grid_has_poles=grid_has_poles)
+            pad_poles_3D = PolarPad3d((1, 1), num_lat = num_lat, grid_has_poles=grid_has_poles)
+        else:
+            self.input_conv = nn.Sequential(
+                nn.ZeroPad2d((0, 0, 1, 1)),
+                nn.CircularPad2d((1, 1, 0, 0)),
+                nn.Conv2d(in_chans, num_levels*hidden_dim, kernel_size=1, stride=1, padding=0)
+            )
+            pad_poles_2D = nn.ZeroPad2d((0, 0, 1, 1))
+            pad_poles_3D = nn.ZeroPad2d((0, 0, 1, 1, 1, 1))
+        pad_circular_2D = nn.CircularPad2d((1, 1, 0, 0))
+        pad_circular_3D = nn.CircularPad3d((1, 1, 0, 0, 0, 0))
+        self.input_conv = nn.Sequential(pad_circular_2D, 
+                                        pad_poles_2D, 
+                                        nn.Conv2d(in_chans, num_levels*hidden_dim, kernel_size=1, stride=1, padding=0))
         self.interp = Interpolate(scale_factor=2, mode="bilinear", align_corners=True)
 
         self.head = nn.Sequential(
-            nn.Conv3d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=1),
+            nn.Conv3d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=0),
             nn.GELU(),
             nn.Conv3d(hidden_dim, hidden_dim, kernel_size=3, stride=1, padding=1), # kernel size 3 for interactions and smoothing
             nn.GELU(),
@@ -322,3 +624,39 @@ class PolarPad3d(nn.Module):
         padded_x[..., :self.pad_top, :] = torch.roll(padded_x[..., :self.pad_top, :], padded_x.shape[-1] // 2, dims = -1)
         padded_x[..., -self.pad_bottom:, :] = torch.roll(padded_x[..., -self.pad_bottom:, :], padded_x.shape[-1] // 2, dims = -1)
         return padded_x
+    
+class Interpolate(nn.Module):
+    """Interpolation module."""
+
+    def __init__(self, scale_factor, mode, align_corners=False):
+        """Init.
+
+        Args:
+            scale_factor (float): scaling
+            mode (str): interpolation mode
+        """
+        super(Interpolate, self).__init__()
+
+        self.interp = nn.functional.interpolate
+        self.scale_factor = scale_factor
+        self.mode = mode
+        self.align_corners = align_corners
+
+    def forward(self, x):
+        """Forward pass.
+
+        Args:
+            x (tensor): input
+
+        Returns:
+            tensor: interpolated data
+        """
+
+        x = self.interp(
+            x,
+            scale_factor=self.scale_factor,
+            mode=self.mode,
+            align_corners=self.align_corners,
+        )
+
+        return x