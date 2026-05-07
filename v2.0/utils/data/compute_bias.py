df3a4947f553cd0a769930f391156f887fb3eccc
Alexander Wikner <awikner1@umd.edu>
2026-05-07 08:57:43 -0600

    Updated compute_bias.py

--- v2.0/utils/data/compute_bias.py
+++ v2.0/utils/data/compute_bias.py
@@ -1,4 +1,4 @@
-#!/work2/09979/awikner/stampede3/miniconda3/envs/emul4climate_nompi/bin/python -u
+#!/usr/bin/env python -u
 #SBATCH -p pvc
 #SBATCH -t 8:00:00
 #SBATCH -N 1
@@ -12,68 +12,135 @@
 
 import xarray as xr
 import numpy as np
-import os, glob
+import os
 from multiprocessing import Pool
 from tqdm import tqdm
 import warnings
 
-def load_var(args):
-    file, var = args
-    with warnings.catch_warnings():
-        warnings.filterwarnings("ignore", message='^.*Unable to decode time axis into full numpy.datetime64 objects.*$')
-        with xr.open_dataset(file) as ds:
-            data = ds[var].values.astype(np.float32)
-    return data
 
-data_dir = '/scratch/09979/awikner/PLASIM/data/2100_year_sims_rerun/sim52/'
-bias_dir = os.path.join(data_dir, 'bias')
+def compute_year_stats(args):
+    """
+    Load one year file and return per-slice statistics without keeping the full
+    time series in memory.
+
+    Parameters
+    ----------
+    args : (file, var, slice_offsets)
+        file          : path to the NetCDF year file
+        var           : variable name to load
+        slice_offsets : list of (step, start) tuples; timesteps are selected
+                        as data[start::step, ...]
+
+    Returns
+    -------
+    list of (sum_array, count) for each slice_offset, or None if var is absent.
+    sum_array is float64 to avoid precision loss when accumulating across years.
+    """
+    file, var, slice_offsets = args
+    with warnings.catch_warnings():
+        warnings.filterwarnings(
+            "ignore",
+            message=r"^.*Unable to decode time axis into full numpy\.datetime64 objects.*$")
+        with xr.open_dataset(file) as ds:
+            if var not in ds:
+                return None
+            data = ds[var].values.astype(np.float32)
+
+    results = []
+    for step, start in slice_offsets:
+        sliced = data[start::step]
+        results.append((sliced.sum(axis=0, dtype=np.float64), sliced.shape[0]))
+    return results
+
+
+data_dir = '/glade/derecho/scratch/awikner/PLASIM/data/2100_year_sims_rerun/sim52'
+bias_dir = os.path.join(data_dir, 'bias2')
 os.makedirs(bias_dir, exist_ok=True)
 years = list(range(12, 132))
 atm_vars = ['ta', 'ua', 'va', 'hus', 'zg']
-slice_offsets = [(1,0), (4,0), (4,1), (4,2), (4,3)]
-slice_names = ['', '_0z','_6z','_12z','_18z']
+
+# Each tuple is (step, start): selects data[start::step, ...].
+# Original code had a bug: data[offset[0]::offset[0]] used [0] for both start
+# and step, so slice_offset[1] was never used and all four synoptic-hour slices
+# produced the same result.  Fixed: data[start::step] = data[offset[1]::offset[0]].
+slice_offsets = [(1, 0), (4, 0), (4, 1), (4, 2), (4, 3)]
+slice_names   = ['',    '_0z', '_6z', '_12z', '_18z']
+
+
+def accumulate(pool, args_in, desc):
+    """
+    Run compute_year_stats in parallel and accumulate weighted sums across years.
+
+    Returns (total_sums, total_counts) where total_sums[i] is the float64 sum
+    of all selected timesteps for slice i across all years, and total_counts[i]
+    is the total number of those timesteps.
+    """
+    total_sums   = None
+    total_counts = None
+
+    for year_stats in tqdm(pool.imap(compute_year_stats, args_in),
+                           total=len(args_in), desc=desc):
+        if year_stats is None:
+            continue
+        if total_sums is None:
+            total_sums   = [np.zeros_like(s, dtype=np.float64) for s, _ in year_stats]
+            total_counts = np.zeros(len(year_stats), dtype=np.int64)
+        for i, (s, n) in enumerate(year_stats):
+            total_sums[i]   += s
+            total_counts[i] += n
+
+    return total_sums, total_counts
+
 
 if __name__ == '__main__':
-    plev_files = [os.path.join(data_dir, 'plev_data', f'{year}_gaussian.nc') for year in years]
-    sigma_files = [os.path.join(data_dir, 'sigma_data', f'{year}_gaussian.nc') for year in years]
+    plev_files  = [os.path.join(data_dir, 'plev_data',  f'{y}_gaussian.nc') for y in years]
+    sigma_files = [os.path.join(data_dir, 'sigma_data', f'{y}_gaussian.nc') for y in years]
+
     with warnings.catch_warnings():
-        warnings.filterwarnings("ignore", message='^.*Unable to decode time axis into full numpy.datetime64 objects.*$')
+        warnings.filterwarnings(
+            "ignore",
+            message=r"^.*Unable to decode time axis into full numpy\.datetime64 objects.*$")
         with xr.open_dataset(plev_files[0]) as ds:
             data_vars = list(ds.data_vars)
         with xr.open_dataset(sigma_files[0]) as ds:
-            lev = ds.lev
+            lev  = ds.lev
             plev = ds.plev
-            data_vars = list(set(list(ds.data_vars) + data_vars) - set(['time_bnds']))
-    print(data_vars)
-    pool = Pool()
+            data_vars = list(set(data_vars + list(ds.data_vars)) - {'time_bnds'})
+
+    print('Variables:', data_vars)
+
+    pool = Pool(16)
     for var in data_vars:
         if var in atm_vars:
             for levels, files in zip([plev, lev], [plev_files, sigma_files]):
-                if var != 'zg' or levels.name != 'lev':
-                    args_in = [(file, var) for file in files]
-                    data = np.concat(list(tqdm(pool.imap(load_var, args_in),
-                                            total = len(files),
-                                            desc = f'Loading {var} {levels.name}')), axis = 0)
-                    for slice_offset, slice_name in tqdm(zip(slice_offsets, slice_names),
-                                                         total = len(slice_names),
-                                                         desc = f'Saving slice {slice_offset}'):
-                        bias = data[slice_offset[0]::slice_offset[0]].mean(axis = 0)
-                        for i, level in enumerate(levels.values):
-                            np.save(os.path.join(bias_dir, f'{var}_{float(level)}_bias{slice_name}.npy'),
-                                    bias[i])
+                if var == 'zg' and levels.name == 'lev':
+                    continue  # zg not on sigma levels
+
+                args_in = [(f, var, slice_offsets) for f in files]
+                total_sums, total_counts = accumulate(
+                    pool, args_in, desc=f'{var}/{levels.name}')
+
+                if total_sums is None:
+                    print(f'WARNING: no data found for {var}/{levels.name}, skipping.')
+                    continue
+
+                for i, (slice_name, count) in enumerate(zip(slice_names, total_counts)):
+                    bias = (total_sums[i] / count).astype(np.float32)
+                    for j, level in enumerate(levels.values):
+                        np.save(
+                            os.path.join(bias_dir, f'{var}_{float(level)}_bias{slice_name}.npy'),
+                            bias[j])
         else:
-            args_in = [(file, var) for file in plev_files]
-            data = np.concat(list(tqdm(pool.imap(load_var, args_in),
-                                            total = len(plev_files),
-                                            desc = f'Loading {var}')), axis = 0)
-            for slice_offset, slice_name in tqdm(zip(slice_offsets, slice_names),
-                                                         total = len(slice_names),
-                                                         desc = f'Saving slices'):
-                bias = data[slice_offset[0]::slice_offset[0]].mean(axis = 0)
+            args_in = [(f, var, slice_offsets) for f in plev_files]
+            total_sums, total_counts = accumulate(pool, args_in, desc=var)
+
+            if total_sums is None:
+                print(f'WARNING: no data found for {var}, skipping.')
+                continue
+
+            for i, (slice_name, count) in enumerate(zip(slice_names, total_counts)):
+                bias = (total_sums[i] / count).astype(np.float32)
                 np.save(os.path.join(bias_dir, f'{var}_bias{slice_name}.npy'), bias)
-            
-            
-                
 
-
-    
+    pool.close()
+    pool.join()
